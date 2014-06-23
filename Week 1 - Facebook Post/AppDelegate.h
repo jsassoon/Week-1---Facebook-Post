38cf331fef4a6436fcc7afe92dfdbd9ba1287870
jsassoon <jsassoon@jsassoon-macbookpro4.roam.corp.google.com>
2014-06-23 10:18:56 -0700

    Initial Commit

---
+++ Week 1 - Facebook Post/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Week 1 - Facebook Post
+//
+//  Created by Josh Sassoon on 6/23/14.
+//  Copyright (c) 2014 youtubeux. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Week 1 - Facebook PostTests/Week_1___Facebook_PostTests.m
@@ -0,0 +1,34 @@
+//
+//  Week_1___Facebook_PostTests.m
+//  Week 1 - Facebook PostTests
+//
+//  Created by Josh Sassoon on 6/23/14.
+//  Copyright (c) 2014 youtubeux. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Week_1___Facebook_PostTests : XCTestCase
+
+@end
+
+@implementation Week_1___Facebook_PostTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
